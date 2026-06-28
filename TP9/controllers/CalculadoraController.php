df9055354b49efe330e1c48e53865316457ffeb1
Gonzalo Corvalan <gonzalo.corvalan77@gmail.com>
2026-06-28 02:06:03 -0300

    se elimino cli

--- TP9/controllers/CalculadoraController.php
+++ TP9/controllers/CalculadoraController.php
@@ -1,9 +1,10 @@
 <?php
 
 require_once './operaciones/Operaciones.php';
-require_once './utils/menus.php';
+require_once './utils/menues.php';
 require_once './utils/pedirNumero.php';
 require_once './utils/limpiarConsola.php';
+
 
 class CalculadoraController
 {
