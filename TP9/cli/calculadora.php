e187d119d867fc7bc004b74b5bb487fc6a4688bf
Gonzalo Corvalan <gonzalo.corvalan77@gmail.com>
2026-06-29 17:32:05 -0300

    require include include_once

--- TP9/cli/calculadora.php
+++ TP9/cli/calculadora.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once './operaciones/Operaciones.php';
-require_once './controllers/ejecutarOperacion.php';
-require_once './utils/menues.php';
+require './operaciones/Operaciones.php';
+include './controllers/ejecutarOperacion.php';
+include_once './utils/menues.php';
 require_once './utils/limpiarConsola.php';
 require_once './utils/pedirNumero.php';
 
